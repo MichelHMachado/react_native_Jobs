93048218cb5fe25a774e5dc001dd59b7789d0e1f
Michel Machado <michelhmachado@gmail.com>
2023-12-28 14:47:17 -0300

    feat: add authentication

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from "react";
+
 import { initializeApp } from "firebase/app";
-
-// Optionally import the services that you want to use
-import { getAuth } from "firebase/auth";
-import db from "firebase/database";
+import {
+  initializeAuth,
+  getReactNativePersistence,
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import functions from "firebase/functions";
-import storage from "firebase/storage";
+import {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from "@firebase/storage";
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-// Initialize Firebase
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -21,12 +33,46 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+
 const firestore = getFirestore(app);
 
+export function signup(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export function login(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function logout() {
+  return signOut(auth);
+}
+
+export function useAuth() {
+  const [currentUser, setCurrentUser] = useState();
+
+  useEffect(() => {
+    const unsub = onAuthStateChanged(auth, (user) => setCurrentUser(user));
+    return unsub;
+  }, []);
+
+  return currentUser;
+}
+
+export async function upload(file, currentUser, setLoading) {
+  const fileRef = ref(getStorage(app), currentUser.uid + ".png");
+
+  setLoading(true);
+
+  const snapshot = await uploadBytes(fileRef, file);
+  const photoURL = await getDownloadURL(fileRef);
+
+  // Workaround to update the profile without triggering getIdToken error
+  await new Promise((resolve) => setTimeout(resolve, 0));
+  await updateProfile(auth.currentUser, { photoURL });
+
+  setLoading(false);
+  alert("Uploaded file!");
+}
+
 export { auth, firestore, app };
-
-// For more information on how to access Firebase in your project,
-// see the Firebase documentation: https://firebase.google.com/docs/web/setup#access-firebase
-
-// IOS: 360299435811-8ofq15g7aqe188liln5mobfeiq9a3hdl.apps.googleusercontent.com
-// Android: 360299435811-72t6j277pdsmj62glps7qjs1v73ct47u.apps.googleusercontent.com
